1626f1647b7198c44b804e1d5f4aa4dc5cc4b094
Connor Jones <ConnorLeeJones@gmail.com>
2019-07-08 16:48:52 -0400

    ouch

--- src/main/java/io/zipcoder/GroceryReporter.java
+++ src/main/java/io/zipcoder/GroceryReporter.java
@@ -1,16 +1,102 @@
 package io.zipcoder;
 
 import io.zipcoder.utils.FileReader;
+import io.zipcoder.utils.Item;
+import io.zipcoder.utils.ItemParseException;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class GroceryReporter {
     private final String originalFileText;
+    private final ItemParser parser = new ItemParser();
+    List<Item> firstList;
+    List<Item> groceryList;
+    LinkedHashMap<String, Integer> itemOccurances = new LinkedHashMap<>();
+    LinkedHashMap<String, List<Double>> priceMap = new LinkedHashMap<>();
+    Integer errors;
+
 
     public GroceryReporter(String jerksonFileName) {
         this.originalFileText = FileReader.readFile(jerksonFileName);
+        this.groceryList = parser.parseItemList(originalFileText);
+        this.errors = parser.getErrors();
+        getMaps();
     }
+
+    public void getMaps(){
+        for (Item item : groceryList){
+            this.itemOccurances.merge(item.getName(), 1, Integer::sum);
+            if (priceMap.containsKey(item.getName())){
+                priceMap.get(item.getName()).add(item.getPrice());
+            } else {
+                List<Double> itemList = new ArrayList<>();
+                itemList.add(item.getPrice());
+                priceMap.put(item.getName(), itemList);
+            }
+        }
+    }
+
+    public String itemToString(Item item) {
+        List<Double> prices = priceMap.get(item.getName());
+        Collections.sort(prices, Collections.reverseOrder());
+        LinkedHashMap<Double, Integer> priceOccurances = new LinkedHashMap<>();
+        for (Double price : prices) {
+            priceOccurances.merge(price, 1, Integer::sum);
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        StringBuilder foodName = new StringBuilder();
+        foodName.append(Character.toUpperCase(item.getName().charAt(0)));
+        foodName.append(item.getName().substring(1));
+
+        result.append(String.format("name:%8s \t\t seen: %1d times\n", foodName.toString(), itemOccurances.get(item.getName())));
+        result.append("============= \t \t =============\n");
+
+        Integer count;
+        for (Map.Entry<Double, Integer> entry : priceOccurances.entrySet()) {
+
+            result.append(String.format("Price: \t %3.2f\t\t seen: %1d times\n", entry.getKey(), entry.getValue()));
+//            if (entry.getValue() == 2) {
+                result.append("-------------\t\t -------------\n");
+        }
+
+        result.append("\n");
+
+
+
+        return result.toString();
+    }
+
+
+
 
     @Override
     public String toString() {
-        return null;
+        StringBuilder result = new StringBuilder();
+
+
+
+        for (Item grocery : groceryList){
+            StringBuilder foodName = new StringBuilder();
+            foodName.append(Character.toUpperCase(grocery.getName().charAt(0)));
+            foodName.append(grocery.getName().substring(1));
+
+
+            if (!(result.toString().contains(foodName))) {
+               result.append(itemToString(grocery));
+           }
+
+
+        }
+
+        String errs = String.format("Errors         \t \t seen: %1d times", + errors);
+        result.append(errs);
+
+        return result.toString();
     }
 }
--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -3,14 +3,75 @@ package io.zipcoder;
 import io.zipcoder.utils.Item;
 import io.zipcoder.utils.ItemParseException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ItemParser {
+
+
+    private Integer errors = 0;
+
     public List<Item> parseItemList(String valueToParse) {
-        return null;
+        ArrayList<Item> itemList = new ArrayList<>();
+        String[] items = valueToParse.split("##");
+        for (String item : items){
+            try {
+                itemList.add(parseSingleItem(item));
+            } catch (ItemParseException e){
+                e.printStackTrace();
+            }
+        }
+        return itemList;
     }
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
-        return null;
+
+        Pattern pattern;
+        Matcher matcher;
+
+
+
+        singleItem = singleItem.replace("##", "");
+            String[] pairs = singleItem.split("[;!]");
+            String[] splitPairs;
+            ArrayList<String> pairList = new ArrayList<>();
+
+
+            for (String pair : pairs) {
+                splitPairs = pair.split("[:@^*%]");
+                for (String value : splitPairs) {
+                    pattern = Pattern.compile("\\s");
+                    matcher = pattern.matcher(value);
+                    if (matcher.find()){}
+                    else {
+                        pairList.add(value);
+                    }
+                }
+            }
+            if (pairList.size() != 8){
+                errors++;
+                throw new ItemParseException();
+            }
+
+
+
+
+
+            pattern = Pattern.compile("0");
+            matcher = pattern.matcher(pairList.get(1));
+            String name = matcher.replaceAll("o");
+
+
+
+
+        return new Item(name.toLowerCase(), Double.valueOf(pairList.get(3)),
+                    pairList.get(5).toLowerCase(), pairList.get(7).toLowerCase());
+
+    }
+
+    public Integer getErrors() {
+        return errors;
     }
 }
